4f1d80de88a546f6cf38b5a64df3536aeffc1d4a
PawelK <pawel.kluska256@gmail.com>
2023-01-13 23:34:55 +0100

    init project

---
+++ src/main/java/com/example/swups/SwupsApplication.java
@@ -0,0 +1,12 @@
+package com.example.swups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SwupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwupsApplication.class, args);
+	}
+
+}
