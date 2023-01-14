fe9d14890e206b20bed651f24f1f5932b8859170
PawelK <pawel.kluska256@gmail.com>
2023-01-14 20:47:08 +0100

    Fixed PlansOfStudies template

--- src/main/java/com/example/swups/controller/PlansOfStudiesController.java
+++ src/main/java/com/example/swups/controller/PlansOfStudiesController.java
@@ -1,13 +1,17 @@
 package com.example.swups.controller;
 
 import com.example.swups.entity.Faculty;
+import com.example.swups.entity.Plansofstudy;
 import com.example.swups.service.PlansOfStudiesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/plans")
@@ -21,9 +25,9 @@ public class PlansOfStudiesController {
         return "plansOfStudies/plans";
     }
 
-    @GetMapping("/details/{id}")
+    @GetMapping("/{id}/details")
     public String getPlanDetails(Model model, @PathVariable String id) {
-        model.addAttribute("plansList", plansOfStudiesService.getPlanOfStudiesById(Long.parseLong(id)));
+        model.addAttribute("plan", plansOfStudiesService.getPlanOfStudiesById(Long.parseLong(id)));
         return "plansOfStudies/details";
     }
 }
