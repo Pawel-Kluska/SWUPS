e90d01220ce306611d90d1b2ba04c5613ff45c2a
PawelK <pawel.kluska256@gmail.com>
2023-01-30 11:40:40 +0100

    navbar changes

--- src/main/java/com/example/swups/config/SecurityConfig.java
+++ src/main/java/com/example/swups/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/")
+                .defaultSuccessUrl("/plans")
                 .permitAll()
                 .and()
                 .logout()
